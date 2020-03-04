6098592b8609ad20717d5a9a217b94e65300108f
Zoe <zdaokroe@sfu.ca>
2020-03-04 15:46:18 -0800

    Added events calendar to homepage

---
+++ script/main.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const btnR = document.querySelector('.btn-right');
+const btnL = document.querySelector('.btn-left');
+const tracks = document.querySelector('.tracks');
+const tracksW = tracks.offsetWidth;
+
+btnR.addEventListener('click', _ => {
+  tracks.scrollBy({
+    left: tracksW / 2,
+    behavior: 'smooth'
+  });
+});
+
+btnL.addEventListener('click', _ => {
+  tracks.scrollBy({
+    left: -tracksW / 2,
+    behavior: 'smooth'
+  });
+});
+
+
+let observer = new IntersectionObserver(entries => {
+  if (!entries[0].isIntersecting) {
+    document.body.classList.add("reveal");
+  } else {
+    document.body.classList.remove("reveal");
+  }
+});
+observer.observe(document.querySelector("#top-of-site-pixel-anchor"));
